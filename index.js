4c7bdc8b301dc8196ece1a1eac8a743667019ce3
liisatormakangas <liisa.tormakangas@gmail.fi>
2023-11-26 17:34:05 +0200

    parcel routing and get parcels added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cabinet_controller_1 = __importDefault(require("./controllers/cabinet_controller"));
+const parcel_controller_1 = __importDefault(require("./controllers/parcel_controller"));
 dotenv_1.default.config();
 const PORT = 3001;
 const app = (0, express_1.default)();
@@ -14,6 +15,7 @@ app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use('/cabinet', cabinet_controller_1.default);
+app.use('/parcel', parcel_controller_1.default);
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
